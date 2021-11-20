313f8dcb4b32858f8cee7cca15f45795ffa27f6b
FunNoober <71403430+FunNoober@users.noreply.github.com>
2021-11-20 09:59:17 -0500

    Made the Website Better
    Got the website working by fixing the Navbar. Now you can actually use the site.

--- elements.js
+++ elements.js
@@ -10,10 +10,6 @@ class FNHEADER extends HTMLElement{
             --clr-link: #00d3cc;
           }
           
-          p {
-            color: var(--clr-primary);
-          }
-          
           .navigation {
             height: 100%;
             display: flex;
@@ -75,62 +71,6 @@ class FNHEADER extends HTMLElement{
             text-decoration: none;
             background-color: rgba(0, 0, 0, 0.5);
           }
-          
-          * {
-            margin: 0;
-            padding: 0;
-          }
-          
-          html,
-          body {
-            margin: 0;
-            height: 100%;
-            width: 100%;
-          
-            font-family: Arial, Helvetica, sans-serif;
-          }
-          
-          body {
-            font-family: Arial, Helvetica, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: var(--clr-lbg);
-            display: flex;
-            flex-direction: column;
-          
-            width: 100%;
-            height: 100%;
-          }
-          
-          h1,
-          h2,
-          h3,
-          h4,
-          h5 {
-            color: var(--clr-primary);
-          }
-          
-          a {
-            color: var(--clr-link);
-            text-decoration: none;
-          }
-          
-          a:hover {
-            font-size: 155%;
-            text-decoration: underline;
-          }
-          
-          .page_content {
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            text-align: center;
-            width: 100%;
-            background-image: linear-gradient(to top, var(--clr-bg), var(--clr-lbg));
-            flex-direction: column;
-            flex: 1;
-          }
           </style>
 
         <div class="navigation">
@@ -144,9 +84,6 @@ class FNHEADER extends HTMLElement{
                 </li>
                 <li class="navbar_item">
                     <a class="navbar_link" href="/Pages/Software.html">Software</a>
-                </li>
-                <li class="navbar_item">
-                    <a class="navbar_link" href="/Pages/UnityPackages.html">Unity Packages</a>
                 </li>
                 <li class="navbar_item">
                     <a class="navbar_link" href="/Pages/Tutorials.html">Tutorials</a>
